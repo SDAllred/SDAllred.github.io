9a203367330e6c8a3529e311ddd171672e69cae0
Chondor <iamspock09@gmail.com>
2015-12-04 12:50:13 -0700

    added a remove schools button

--- js/SchoolsList.js
+++ js/SchoolsList.js
@@ -417,6 +417,43 @@ var SchoolsList = function(cVis) {
 		
 		});
 		
+		
+		
+		$("#SSRemoveButton").click(function(){
+			
+			var extractedRows = [], left, right;
+			var rows = grid.getSelectedRows();
+			var insertBefore = 0;
+			left = sData.slice(0, insertBefore);
+			right = sData.slice(insertBefore, sData.length);
+			rows.sort(function(a,b) { return a-b; });
+			for (var i = 0; i < rows.length; i++) {
+			  extractedRows.push(sData[rows[i]]);
+			}
+			rows.reverse();
+			for (var i = 0; i < rows.length; i++) {
+			  var row = rows[i];
+			  if (row < insertBefore) {
+				left.splice(row, 1);
+			  } else {
+				right.splice(row - insertBefore, 1);
+			  }
+			}
+			sData = left.concat(right);
+			var selectedRows = [];
+			//for (var i = 0; i < rows.length; i++)
+			//  selectedRows.push(left.length + i);
+			grid.resetActiveCell();
+			grid.setData(sData);
+			grid.setSelectedRows(selectedRows);
+			grid.render();
+			
+			// update the graphs with the new list
+			cVis.fSchools = sData;
+			cVis.updateGraphs(sData);
+		
+		});
+		
 	}
 
 	
